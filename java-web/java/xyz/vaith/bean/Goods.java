733190e5d76a71922d3eb8953eb06311e00314db
Vaith <vaithwee@yeah.net>
2018-12-16 10:04:53 +0800

    Add index resource and servlet

---
+++ java-web/java/xyz/vaith/bean/Goods.java
@@ -0,0 +1,100 @@
+package xyz.vaith.bean;
+
+public class Goods {
+    private int id;
+    private String name;
+    private String cover;
+    private String image1;
+    private String image2;
+    private float price;
+    private String intro;
+    private int stock;
+    private int type_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getImage1() {
+        return image1;
+    }
+
+    public void setImage1(String image1) {
+        this.image1 = image1;
+    }
+
+    public String getImage2() {
+        return image2;
+    }
+
+    public void setImage2(String image2) {
+        this.image2 = image2;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cover='" + cover + '\'' +
+                ", image1='" + image1 + '\'' +
+                ", image2='" + image2 + '\'' +
+                ", price=" + price +
+                ", intro='" + intro + '\'' +
+                ", stock=" + stock +
+                ", type_id=" + type_id +
+                '}';
+    }
+}
--- java-web/test/C3p0Test.java
+++ java-web/test/C3p0Test.java
@@ -1,7 +1,11 @@
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.MapHandler;
 import org.junit.Test;
+import xyz.vaith.bean.Goods;
+import xyz.vaith.util.DBUtil;
 
 
 import javax.sql.DataSource;
@@ -28,5 +32,20 @@ public class C3p0Test {
         QueryRunner runner = new QueryRunner(dataSource);
         Map map = runner.query("select * from goods where id = 5", new MapHandler());
         System.out.println(map);
+    }
+
+    @Test
+    public void queryBean() throws Exception {
+        DataSource dataSource = new ComboPooledDataSource();
+        QueryRunner runner = new QueryRunner(dataSource);
+        Goods goods = runner.query("select * from goods where id = 5", new BeanHandler<Goods>(Goods.class));
+        System.out.println(goods);
+    }
+
+    @Test
+    public void testDBUitl() throws Exception {
+        QueryRunner runner = new QueryRunner(DBUtil.getDataSource());
+        Goods goods = runner.query("select * from goods where id = 5", new BeanHandler<Goods>(Goods.class));
+        System.out.println(goods);
     }
 }
